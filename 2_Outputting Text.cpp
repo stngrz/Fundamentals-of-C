e30eedb9e3ebf0fabcc61273ca77cb8c6ebf017f
Jacob Kolb <jkolb12@gmail.com>
2017-06-10 19:35:46 -0600

    Add files via upload

---
+++ 2_Outputting Text.cpp
@@ -0,0 +1,16 @@
+//outputting text
+#include <iostream>//input output stream
+
+using namespace std; //standard
+
+int main(){// a subroutine a function
+	//cout to output \ endl flushes the buffer clearing out any sititng text
+	cout <<"Starting Program..."<<flush;
+
+	cout <<"This is some text."<< endl;
+
+	cout <<"Banana. "<<"Apple. "<<"Orange."<<endl;
+
+	cout <<"This is some more text."<<endl;
+	return 0;//a statement
+}
