c72f75edb062260ff52e4047b880330c7e40945e
Jacob Kolb <jkolb12@gmail.com>
2017-06-25 13:17:56 -0600

    Add files via upload
    For C++ designation.

---
+++ 1_Hello World.cpp
@@ -0,0 +1,13 @@
+//Hello World
+//easy peasy
+
+#include <iostream>
+
+using namespace std;//std = standard
+
+int main() {//int = type declaration; main = function or subroutine
+	//cout c-out object \ << insertion operator \ endl creates new line
+	cout << "Hello World :)"<< endl;
+
+	return 0; //0 usually means everything is fine
+}
