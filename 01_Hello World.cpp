0538bcf21526da38c27866d185b66a6b5129aa6c
Jacob Kolb <jkolb12@gmail.com>
2017-06-25 13:49:42 -0600

    Update and rename 1_Hello World.cpp to 01_Hello World.cpp
    Readability edit

--- 01_Hello World.cpp
+++ 01_Hello World.cpp
@@ -6,7 +6,8 @@
 using namespace std;//std = standard
 
 int main() {//int = type declaration; main = function or subroutine
-	//cout c-out object \ << insertion operator \ endl creates new line
+	
+	//(cout) c-out object; (<<) insertion operator; (endl) flushes text buffer and creates new line
 	cout << "Hello World :)"<< endl;
 
 	return 0; //0 usually means everything is fine
